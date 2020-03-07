26c2192e9dc5e301ccba6cc2f1c08dca049e2a50
nicknack <nicknicknick22@aol.com>
2020-03-07 12:22:28 -0500

    Established mysql connection to node & added employee database with tables

---
+++ EmployeeSeeds.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,6) NOT NULL,
+  department_id INT(10) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  manager_id INT(10),
+  PRIMARY KEY (id)
+);
