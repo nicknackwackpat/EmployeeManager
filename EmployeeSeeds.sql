001654bfe435262af73f77becae2726ad8c04732
nicknack <nicknicknick22@aol.com>
2020-03-08 21:59:48 -0400

    Added view employee functions

--- EmployeeSeeds.sql
+++ EmployeeSeeds.sql
@@ -13,15 +13,30 @@ CREATE TABLE department (
 CREATE TABLE role (
   id INT AUTO_INCREMENT NOT NULL,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL(10,6) NOT NULL,
+  salary DECIMAL(13,2) NOT NULL,
   department_id INT(10) NOT NULL,
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id) REFERENCES deptartment(id)
 );
 
-CREATE TABLE employee (
+CREATE TABLE employees (
   id INT AUTO_INCREMENT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   manager_id INT(10),
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employees(id)
 );
+
+INSERT INTO employees (first_name, last_name)
+VALUES ("Vanille", "Oerba"), ("Dina", "Caliente"), ("Nina", "Caliente"), ("Bella", "Goth");
+
+INSERT INTO employees (first_name, last_name, manager_id)
+VALUES ("Caroline", "Custard", 3), ("Tyshawn", "Mercer", 4), ("Ann", "Takamaki", 2), ("Don", "Lothario", 1), ("Chad", "Michaels", 2);
+
+INSERT INTO department (name)
+VALUES ("Marketing"), ("Operations"), ("Finance"), ("Sales"), ("Research"), ("Sim Resources");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Chief Marketing Assistant", 150000.00, 1), ("CEO", 600000.00, 2), ("Vice President", 550000.00, 2), ("Futures Trader", 250000.00, 3), ("Angel Investor", 275000.00, 3), ("Telemarketer", 400000.00, 4), ("Field Sales Representative", 800000.00, 4),("Corporate Drone", 60.000, 5), ("Report Processor", 75.000, 5), ("Custom Content Creator", 175.000, 6);
--- manager.js
+++ manager.js
@@ -1,13 +1,142 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require("console.table");
+const figlet = require("figlet");
+
+figlet('Landgraab Inc.', function (err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  // Your port; if not 3306
-  port: 3306,
-  // Your username
-  user: "root",
-  // Your password
-  password: "Biacxv448!",
-  database: "employee_db"
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Biacxv448!",
+    database: "employee_db"
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    // console.log("connected as id " + connection.threadId + "\n");
+    startPrompt();
+});
+
+function startPrompt() {
+    inquirer.prompt({
+        type: "list",
+        message: "What would you like to do?",
+        name: "startQ",
+        choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", "Add Employee", "Remove Employee", "Update Employee", "Quit"],
+
+    }).then(action => {
+
+
+        switch (action.startQ) {
+            case "View All Employees":
+                viewEmployees();
+            case "View All Employees by Department":
+                viewEmployeesDep();
+            case "View All Employees by Manager":
+                viewEmployeesMang();
+            case "Add Employee":
+                addEmployee();
+            case "Remove Employee":
+                removeEmployee();
+            case "Update Employee":
+                updateEmployee();
+            case "Quit":
+                // connection.end();
+        };
+    });
+};
+
+function viewEmployees() {
+    console.log("-----------------------------------");
+    connection.query("SELECT employees.id, first_name, last_name, title, department_id, salary, manager_id FROM role INNER JOIN employees ON role.id = employees.role_id;", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        console.log("-----------------------------------");
+        // };
+        startPrompt();
+    });
+};
+
+function viewEmployeesDep() {
+    inquirer.prompt({
+        type: "list",
+        message: "Which department would you like to search for?",
+        name: "departmentQ",
+        choices: ["Marketing", "Operations", "Finance", "Sales", "Research", "Sim Resources"]
+
+    }).then(answers => {
+        connection.query("SELECT employees.id, first_name, last_name, title, department_id FROM role INNER JOIN employees ON department.id = role.department_id;", [`${answers.departmentQ}`], function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            // for (var i = 0; i < res.length; i++) {
+            //     console.table(res[i].id + " | " + res[i].name + ");
+            // });
+        });
+    });
+};
+
+function viewEmployeesMang() {
+
+};
+
+function addEmployee() {
+    inquirer.prompt([{
+        type: "input",
+        message: "What is the employee's first name?",
+        name: "employeeFN",
+    }, {
+        type: "input",
+        message: "What is the employee's last name?",
+        name: "employeeLN",
+    }, {
+        type: "list",
+        message: "What is the employee's role?",
+        name: "employeeRole",
+        choices: ["Chief Marketing Assistant", "CEO", "Vice President", "Futures Trader", "Angel Investor", "Telemarketer", "Field Sales Representative", "Corporate Drone", "Report Processor", "Custom Content Creator"]
+    }, {
+        type: "list",
+        message: "Who is the employee's manager?",
+        name: "employeeMang",
+        choices: ["None", "Vanille Oerba", "Dina Caliente", "Nina Caliente", "Bella Goth"]
+
+    }]).then(answers => {
+        if(answers.employeeMang === "None") {
+            connection.query(`INSERT INTO employees (first_name, last_name, role_id)  VALUES ("${answers.employeeFN}", "${answers.employeeLN}, ${answers.employeeRole});`, function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            });
+        } else {
+            connection.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id)  VALUES ("${answers.employeeFN}", "${answers.employeeLN}, ${answers.employeeRole}, ${answers.employeeMang});`, function (err, res) {
+                if (err) throw err;
+                console.table(res);
+            }); 
+        };
+    });
+};
+
+    function removeEmployee() {
+        inquirer.prompt({
+            type: "list",
+            message: "Which employee would you like to remove?",
+            name: "removeQ",
+            choices: ["Vanille Oerba", "Dina Caliente", "Nina Caliente", "Bella Goth", "Caroline Custard", "Tyshawn Mercer", "Ann Takamaki", "Don Lothario", "Chad Michaels"]
+
+        }).then(answers => {
+            connection.query(`DELETE ${answers.removeQ} from employees;`, function (err, res) {
+                if (err) throw err;
+                console.table(res);
+                startPrompt();
+            });
+        });
+    };
+
+    // function updateEmployee() {
